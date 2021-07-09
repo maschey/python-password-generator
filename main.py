c344b99e95f92b5e62f73d0a7c321318934572cb
Maximilian Anderlik <anderlik3@gmail.com>
2021-07-09 02:13:34 +0200

    changed random number generator to secrets because it's able to generate cryptographically strong random numbers

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import secrets
 import string
 import random
 
 password_scaffolding = string.ascii_letters + string.digits + string.punctuation
-
-
 def get_length():
     length = int(input("Length of Password (default 12): ") or "12")
     return int(length)
@@ -11,12 +10,8 @@ def get_length():
 
 def password_generator(length):
     password_scaffolding_list = list(password_scaffolding)
-    random.shuffle(password_scaffolding_list)
 
-    password = random.choices(password_scaffolding_list, k=length)
-
-    password = ''.join(password)
-    return password
+    return ''.join(secrets.choice(password_scaffolding_list) for i in range(length))
 
 
 def main():
